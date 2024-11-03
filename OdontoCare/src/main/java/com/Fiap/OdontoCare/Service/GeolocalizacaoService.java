becf444e19fdc4064039616cbfeaed46a3846aad
Beatriz silva <136511854+bia98silva@users.noreply.github.com>
2024-11-03 19:18:15 -0300

    Add files via upload

---
+++ OdontoCare/src/main/java/com/Fiap/OdontoCare/Service/GeolocalizacaoService.java
@@ -0,0 +1,38 @@
+package com.Fiap.OdontoCare.Service;
+
+import com.Fiap.OdontoCare.DTO.GeolocalizacaoDTO;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
+
+@Service
+public class GeolocalizacaoService {
+
+    private static final String NOMINATIM_URL = "https://nominatim.openstreetmap.org/search?format=json&q=";
+
+    public Optional<GeolocalizacaoDTO> getCoordinates(String address) {
+        String url = NOMINATIM_URL + address.replace(" ", "+");
+
+        RestTemplate restTemplate = new RestTemplate();
+        String response = restTemplate.getForObject(url, String.class);
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(response);
+
+            if (jsonNode.isArray() && jsonNode.size() > 0) {
+                JsonNode firstResult = jsonNode.get(0);
+                double latitude = firstResult.get("lat").asDouble();
+                double longitude = firstResult.get("lon").asDouble();
+                return Optional.of(new GeolocalizacaoDTO(latitude, longitude));
+            }
+        } catch (Exception e) {
+            e.printStackTrace(); // Trate a exceção de acordo com sua estratégia de erro
+        }
+
+        return Optional.empty(); // Retorna vazio se não encontrar coordenadas
+    }
+}
